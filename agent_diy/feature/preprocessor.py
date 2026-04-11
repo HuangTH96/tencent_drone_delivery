b4068bc0807be5138ac90aae569f79b7e5d3b71b
ZhiLu <60755772+HuangTH96@users.noreply.github.com>
2026-04-11 11:49:48 +0800

    feat: update warehouse and npc feat

--- agent_diy/feature/preprocessor.py
+++ agent_diy/feature/preprocessor.py
@@ -58,8 +58,11 @@ class Preprocessor:
 
         # Entities / 实体
         self.stations = []
+        self.chargers = []
+        self.warehouse = None
+        self.npcs = []
 
-    # TODO：修改可以获得的观测信息
+    # 提取观测信息
     def _parse_obs(self, env_obs):
         """Parse essential fields from observation dict.
 
@@ -91,6 +94,7 @@ class Preprocessor:
             else:
                 self.warehouse = organ
 
+        self.npcs =frame_state["npcs"]                          # list of NPCs -> list[NpcState]
         self.legal_act = obs.get("legal_action", [1] * 8)       # 合法动作
 
     # TODO
@@ -122,10 +126,13 @@ class Preprocessor:
         # 2. 按config_id排序填充每个驿站的特征
         # 3. 投递后会自动删除该包裹，比如一开始是[3,6,9]，当投递6后，self.packages会变成 [3,9]；也就是说，驿站的个数会变化
         
+        # TODO: 常量移动至conf.py 中
         TOTAL_STATIONS = 10
         STATION_FEAT_DIM = 5  
         TOTAL_CHARGERS = 4
         CHARGER_FEAT_DIM = 3
+        TOTAL_NPCs = 4
+        NPC_FEAT_DIM = 4
 
         # 统计每个目标驿站对应的包裹数
         # self.packages 是 list[int]，元素为驿站编号，同一驿站可能出现多次；
@@ -133,20 +140,28 @@ class Preprocessor:
         for pkg_id in self.packages:
             pkg_count[pkg_id] = pkg_count.get(pkg_id, 0) + 1
 
+        station_map = {s.get("config_id"): s for s in self.stations}
+
         # 遍历所有驿站（按config_id固定排序）
         station_feat_list = []
-        for station in sorted(self.stations, key=lambda x:x.get("config_id", 0)):
-            sid = station.get("config_id", 0)   # -> OrganState
-            pkg_num = pkg_count.get(sid, 0)
-            # 根据有无包裹判断是否需要对该驿站的特征进行补零操作
-            exist = 1.0 if pkg_num > 0 else 0.0
-            if exist == 1.0:
-                target_pos_s = (station["pos"]["x"], station["pos"]["z"])
-                dir_x_s, dir_z_s, norm_dist_s = _get_target_feature(self.cur_pos, target_pos_s)
-                norm_pkg_num = pkg_count[sid] / 3.0          # [0, 1]
-            else:
-                dir_x_s, dir_z_s, norm_dist_s, norm_pkg_num = 0.0, 0.0, 0.0, 0.0
 
+        for sid in range(1, TOTAL_STATIONS + 1): # sorted(self.stations, key=lambda x:x.get("config_id", 0)):
+            station = station_map.get(sid)   # -> OrganState
+
+            if station is not None:
+                pkg_num = pkg_count.get(sid, 0)
+                # 根据有无包裹判断是否需要对该驿站的特征进行补零操作
+                exist = 1.0 if pkg_num > 0 else 0.0
+                if exist == 1.0:
+                    target_pos_s = (station["pos"]["x"], station["pos"]["z"])
+                    dir_x_s, dir_z_s, norm_dist_s = _get_target_feature(self.cur_pos, target_pos_s)
+                    norm_pkg_num = pkg_count[sid] / 3.0          # [0, 1]
+                else:
+                    dir_x_s, dir_z_s, norm_dist_s, norm_pkg_num = 0.0, 0.0, 0.0, 0.0
+            else: 
+                # 对本局没有的驿站进行补零
+                exist, dir_x_s, dir_z_s, norm_dist_s, norm_pkg_num = 0.0, 0.0, 0.0, 0.0, 0.0
+            
             station_feat_list.append(np.array([exist, dir_x_s, dir_z_s, norm_dist_s, norm_pkg_num]))
 
         station_feat = np.concatenate(station_feat_list)  # 50D
@@ -156,39 +171,52 @@ class Preprocessor:
         # 添加充电站特征
         # == 语义信息：1. 添加所有充电桩（4个），记录中心点与hero方向信息，计算边界到hero的距离信息
         # == dir_x, dir_z, norm_boundary_dist,  # 充电桩1-4
-        # == 其他说明：1. 在特征向量中固定槽位，明确“哪个特征属于哪个实体”；2. 按config_id排序填充每个驿站的特征
+        # == 其他说明：1. 在特征向量中固定槽位，明确“哪个特征属于哪个实体”；2. 按config_id排序填充每个驿站的特征; 3. TODO: 充电桩有充电范围，不应该以充电桩位置为target_pos_c
         charger_feat_list = []  
-        for c in sorted(self.chargers, key=lambda x:x.get("config_id", 0)):
-            target_pos_c = (c["pos"]["x"], c["pos"]["z"])
-            dir_x_c, dir_z_c, norm_dist_c = _get_target_feature(self.cur_pos, target_pos_c)
-            charger_feat_list.append(np.array([dir_x_c, dir_z_c, norm_dist_c]))
+        sorted_chargers = sorted(self.chargers, key=lambda x:x.get("config_id", 0))
+        for i in range(TOTAL_CHARGERS):
+            if i < len(sorted_chargers):
+                c = sorted_chargers[i]
+                target_pos_c = (c["pos"]["x"], c["pos"]["z"])
+                dir_x_c, dir_z_c, norm_dist_c = _get_target_feature(self.cur_pos, target_pos_c)
+                charger_feat_list.append(np.array([dir_x_c, dir_z_c, norm_dist_c]))
+            else:
+                charger_feat_list.append(np.zeros(CHARGER_FEAT_DIM))
 
         charger_feat = np.concatenate(charger_feat_list)    # 12D
         assert len(charger_feat) == TOTAL_CHARGERS * CHARGER_FEAT_DIM, \
             f"charger_feat dim error: expected {TOTAL_CHARGERS * CHARGER_FEAT_DIM}, got {len(charger_feat)}"
 
-        # TODO： 添加仓库信息
+        # 添加仓库信息
         # == 语义信息：1. 计算中心点与hero的相对位置，计算边界到hero的距离
-        # == 其他说明：1. 在特征向量中固定槽位，明确“哪个特征属于哪个实体”；
+        # == 其他说明：1. 在特征向量中固定槽位，明确“哪个特征属于哪个实体”；2. TODO：仓库分装货区域和非装货区域
         # == warehouse_dir_x, warehouse_dir_z, norm_boundary_dist
+        target_pos_w = (self.warehouse["pos"]["x"], self.warehouse["pos"]["z"])
+        dir_x_w, dir_z_w, norm_dist_w = _get_target_feature(self.cur_pos, target_pos_w)
 
+        warehouse_feat = np.array([dir_x_w, dir_z_w, norm_dist_w])
 
-        warehouse_feat = None
-
-
-
-        # TODO：添加NPC信息
+        # 添加NPC信息
         # == 语义信息：1. 计算NPCs 和 hero之间的方向信息和以及归一化的距离信息
-        # == 其他说明：1. 在特征向量中固定槽位，明确“哪个特征属于哪个实体”；2. 按config_id排序填充每个驿站的特征
-        # == exist, dir_x, dir_z, norm_dist_scalar,  # NPC1
-        # == exist, dir_x, dir_z, norm_dist_scalar,  # NPC2
-        # == exist, dir_x, dir_z, norm_dist_scalar,  # NPC3
-        # == exist, dir_x, dir_z, norm_dist_scalar,  # NPC4
-
-
-
-
-        npc_feat = None
+        # == 其他说明：1. 跨局槽位对应无所谓，只需要保证单个episode内，在特征向量中固定槽位，明确“哪个特征属于哪个实体”；2. 按config_id排序填充每个驿站的特征；3. 需要补零，保持特征个数
+        # == exist, dir_x, dir_z, norm_dist_scalar,  # NPC
+        npc_feat_list = []
+        sorted_npcs = sorted(self.npcs, key=lambda x:x.get("npc_id", ""))
+        for i in range(TOTAL_NPCs):
+            if i < len(sorted_npcs):
+                npc =sorted_npcs[i]
+                target_pos_npc = (npc["pos"]["x"], npc["pos"]["z"])
+                exist = 1.0
+                dir_x_npc, dir_z_npc, norm_dist_npc = _get_target_feature(self.cur_pos, target_pos_npc)
+            else:
+                exist = 0.0
+                dir_x_npc, dir_z_npc, norm_dist_npc = 0.0, 0.0, 0.0
+            
+            npc_feat_list.append(np.array([exist, dir_x_npc, dir_z_npc, norm_dist_npc]))
+        
+        npc_feat = np.concatenate(npc_feat_list)    # 16D
+        assert len(npc_feat) == TOTAL_NPCs * NPC_FEAT_DIM, \
+            f"npc_feat dim error: expected {TOTAL_NPCs * NPC_FEAT_DIM}, got {len(npc_feat)}"
 
         # 3. Legal action mask (8D) / 合法动作掩码（8D）
         legal_action = self._get_legal_action()
